80e623dd4d5e10c6b3d9c9020070e4d82b5207b1
Maxlegend87 <dani_69mini@hotmail.es>
2016-11-22 16:53:05 +0100

    Log local

--- modules/usuario.js
+++ modules/usuario.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql');
 
 class usuario {
-    constructor(nombre,mysqlconnect) {
+    constructor(nombre, mysqlconnect) {
             this._nombre = nombre;
             this._mysqlconnection = mysqlconnect;
             this._id = null;
@@ -53,8 +53,8 @@ class usuario {
 
     // ---- Funciones ------ //
     // // const sql = 'INSERT INTO usuarios.usuario (IDred, username, provider, photo, password, email) VALUES ("' + this._id + '", "' + this._nombre + '", "' + this._provider + '", "' + this._foto + '", "' + this._password + '", "' + this._correo + '");';
-    registrarOneClick(cb) {
-        let IDred=this._id+this._provider[0];
+    OneClick(cb) {
+        let IDred = this._id + this._provider[0];
         const comprobar = 'SELECT * FROM usuarios.usuario where IDred = "' + IDred;
         const insertar = 'INSERT INTO usuarios.usuario (IDred, username, provider, photo) VALUES ("' + IDred + '", "' + this._nombre + '", "' + this._provider + '", "' + this._foto + '");';
         let cliente = mysql.createConnection(this._mysqlconnection);
@@ -96,12 +96,32 @@ class usuario {
                     return cb(err, 1);
                 } else {
                     cliente.query(insertar, (err) => {
+                        cliente.end();
                         if (err) {
                             return cb(err, 2);
                         } else {
                             return cb(err, 0);
                         }
                     });
+                }
+            });
+        });
+    }
+    logLocal(cb) {
+        const comprobar = 'SELECT * FROM usuarios.usuario where username = "' + this._nombre + '" and provider = "local"';
+        let cliente = mysql.createConnection(this._mysqlconnection);
+        cliente.connect((err) => {
+            if (err) {
+                return cb(err, 2);
+            }
+            cliente.query(comprobar, (err, rows) => {
+                cliente.end();
+                if (err) {
+                    return cb(err, 2);
+                } else if (rows.length) {
+                    return cb(err, 0);
+                }else{
+                    return cb(err,1)
                 }
             });
         });
