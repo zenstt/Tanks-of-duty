1ce9cb5d4f9349dad956724da75bb86a64cd7ecc
Maxlegend87 <dani_69mini@hotmail.es>
2016-11-28 18:29:18 +0100

    Comentarios mas

--- modules/usuario.js
+++ modules/usuario.js
@@ -57,8 +57,8 @@ class usuario {
      * si no, lo meterá en la base de datos
      * 
      * si no conecta devolvera un error, y un 2:este es un error interno
-     * si conecta, y encuentra que el usuario ya existe devolvera un 1 y la id del usuario en nuesta base de datos
-     * en caso de que no, al meterlo en la base de datos, devolvera un 0 y la id del usuario en nuesta base de datos
+     * si conecta, y encuentra que el usuario ya existe devolvera un 1 y la id del usuario en nuestra base de datos
+     * en caso de que no, al meterlo en la base de datos, devolvera un 0 y la id del usuario en nuestra base de datos
      *
      * 1 y 0 son tecnicamente lo mismo para la callback, pero lo dejo por si es necesario hacer algo con 
      * los usuarios nuevos
@@ -102,7 +102,7 @@ class usuario {
      * 
      * si no conecta devolvera un error, y un 2: este es un error interno
      * si conecta, y encuentra que el usuario ya existe devolvera un 1 : significara que el usuario local ya existe
-     * en caso de que no, al meterlo en la base de datos, devolvera un 0 y la id del usuario en nuesta base de datos
+     * en caso de que no, al meterlo en la base de datos, devolvera un 0 y la id del usuario en nuestra base de datos
      * 
      * @param {Function} cb Callback que hará lo necesario: le devolvemos el error, el codigo dado 
      * y la id en nuestra base de datos
@@ -142,7 +142,7 @@ class usuario {
      * 
      * si no conecta devolvera un error, y un 2: este es un error interno
      * si conecta, y no encuentra el usuario devolvera un 1 : significara que el usuario local no existe
-     * en caso de encontrarlo devolvera un 0 y la id del usuario en nuesta base de datos
+     * en caso de encontrarlo devolvera un 0 y la id del usuario en nuestra base de datos
      * 
      * @param {Function} cb Callback que hará lo necesario: le devolvemos el error, el codigo dado 
      * y la id en nuestra base de datos
@@ -174,7 +174,7 @@ class usuario {
      * si conecta, y no encuentra el usuario devolvera un 1 : significara que el usuario local no existe
      * en caso de encontrarlo devolvera un 0 y la informacion del usuario en nuesta base de datos
      * 
-     * @param  {number}   id id propia de nuestra base de datos
+     * @param  {number}   id id del usuario en nuestra base de datos
      * @param  {Function} cb Callback que hará lo necesario: le devolvemos el error, el codigo dado y los datos del usuario con esa id
      * @return {Function}    cb
      */
@@ -197,6 +197,18 @@ class usuario {
             });
         });
     }
+    /**
+     * Se encarga de editar la info del usuario local en la base de datos por la id
+     *
+     * Hay que dar la info del usuario mediante setters
+     * 
+     * si no conecta devolvera un error, y un 2: este es un error interno
+     * en caso de cambiarlo correctamente devolvera un 0 
+     * 
+     * @param  {number}   id id del usuario en nuestra base de datos
+     * @param  {Function} cb Callback que hará lo necesario: le devolvemos el error y el codigo dado
+     * @return {Function}    cb
+     */
     editarUsuario(id, cb) {
         const modificar = "UPDATE usuarios.usuario SET username='" + this._nombre + "', photo='" + this._foto + "',email='" + this._email + "' WHERE ID='" + id + "'";
         let cliente = mysql.createConnection(this._mysqlconnection);
@@ -214,6 +226,18 @@ class usuario {
             });
         });
     }
+    /**
+     * Se encarga de editar la contraseña del usuario LOCAL en la base de datos por la id
+     *
+     * Hay que dar la contraseña nueva del usuario mediante setters
+     * 
+     * si no conecta devolvera un error, y un 2: este es un error interno
+     * en caso de cambiarla correctamente devolvera un 0 
+     * 
+     * @param  {number}   id id del usuario en nuestra base de datos
+     * @param  {Function} cb Callback que hará lo necesario: le devolvemos el error y el codigo dado
+     * @return {Function}    cb
+     */
     cambiarContra(id, cb) {
         const modificar = "UPDATE usuarios.usuario SET password='" + this._password + "' WHERE ID='" + id + "'";
         let cliente = mysql.createConnection(this._mysqlconnection);
@@ -231,6 +255,17 @@ class usuario {
             });
         });
     }
+    /**
+     * Se encarga de consultar los tanques que tiene el jugador dado por el id
+     * 
+     * si no conecta devolvera un error, y un 2: este es un error interno
+     * si conecta, y no encuentra tanques devolvera un 1 : significara que el usuario no tiene tanques
+     * en caso de encontrar correctamente devolvera un 0 y la informacion de los tanques del usuario
+     * 
+     * @param  {number}   id id del usuario en nuestra base de datos
+     * @param  {Function} cb Callback que hará lo necesario: le devolvemos el error , el codigo dado y todos los tanques en un array
+     * @return {Function}    cb
+     */
     consultarTanques(id, cb) {
         const buscar = 'SELECT (ID,nombre,hp,ammo,urlia) FROM usuarios.tanque where usuario = "' + id + '"';
         let cliente = mysql.createConnection(this._mysqlconnection);
@@ -250,6 +285,17 @@ class usuario {
             });
         });
     }
+    /**
+     * Se encarga de crear un tanque para el jugador dado por el id
+     * 
+     * si no conecta devolvera un error, y un 2: este es un error interno
+     * en caso de insertar correctamente devolvera un 0
+     * 
+     * @param  {number}   id id del usuario en nuestra base de datos
+     * @param  {Object}   tanque un objeto tanque creado con los datos necesarios
+     * @param  {Function} cb Callback que hará lo necesario: le devolvemos el error y el codigo dado
+     * @return {Function}    cb
+     */
     crearTanque(id, tanque, cb) {
         const crear = "INSERT INTO usuarios.tanque (nombre, hp, ammo, urlia, usuario) VALUES ('" + tanque.nombre + "', '" + tanque.vida + "', '" + tanque.muni + "', '" + tanque.IA + "', '" + id + "');";
         let cliente = mysql.createConnection(this._mysqlconnection);
@@ -267,6 +313,17 @@ class usuario {
             });
         });
     }
+    /**
+     * Se encarga de borrar el tanque que tiene el id dado para el jugador dado por el id
+     * 
+     * si no conecta devolvera un error, y un 2: este es un error interno
+     * en caso de borrar correctamente devolvera un 0
+     * 
+     * @param  {number}   id id del usuario en nuestra base de datos
+     * @param  {number}   idtanque id del tanque en nuestra base de datos
+     * @param  {Function} cb Callback que hará lo necesario: le devolvemos el error y el codigo dado
+     * @return {Function}    cb
+     */
     borrarTanque(id, idtanque, cb) {
         const borrar = "DELETE FROM usuarios.tanque WHERE ID='" + idtanque + "' and usuario='" + id + "';";
         let cliente = mysql.createConnection(this._mysqlconnection);
@@ -284,6 +341,17 @@ class usuario {
             });
         });
     }
+    /**
+     * Se encarga de modificar el tanque con el id mediante un objeto tanque para el jugador dado por el id
+     * 
+     * si no conecta devolvera un error, y un 2: este es un error interno
+     * en caso de borrar correctamente devolvera un 0
+     * 
+     * @param  {number}   id id del usuario en nuestra base de datos
+     * @param  {Object}   tanque un objeto tanque creado con los datos nuevos
+     * @param  {Function} cb Callback que hará lo necesario: le devolvemos el error y el codigo dado
+     * @return {Function}    cb
+     */
     modificarTanque(id, tanque, idtanque, cb) {
         const modificar = "UPDATE usuarios.tanque SET nombre='" + tanque.nombre + "', hp='" + tanque.vida + "', ammo='" + tanque.muni + "', urlia='" + tanque.IA + "' WHERE ID='" + idtanque + "' and usuario='" + id + "';";
         let cliente = mysql.createConnection(this._mysqlconnection);
