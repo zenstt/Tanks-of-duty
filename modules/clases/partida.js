39e24a4a45b9727333d0bf92a7deab8f945fc793
Maxlegend87 <dani_69mini@hotmail.es>
2017-02-20 20:14:49 +0100

    Comentarios en partida

--- modules/clases/partida.js
+++ modules/clases/partida.js
@@ -6,95 +6,84 @@ const assert = require('assert');
 const urlMongo = require('../config/conf.js').mongo.url;
 
 class Partida {
-
-	constructor(id, nombre, columnas, filas) {
+	/**
+	 * Crea un tanque con un nombre que servirá como id
+	 * @param  {string} nombre nombre del tanque
+	 * @param  {string} o orientación del tanque, si no se da este parámetro, se pondrá una orientación aleatoria
+	 * @param  {number} vida vida del tanque, si no se da este parámetro, se pondrá en 10
+	 * @param  {number} muni munición máxima del tanque, si no se da este parámetro, se pondrá en 50
+	 * 
+	 * la posición se le dará al insertarla en una casilla
+	 * x representa la posición segun las columnas
+	 * y representa la posición segun las filas
+	 */
+	constructor(id, nombre, medida) {
 		this._id = id;
 		this._nombre = nombre;
-		this._tablero = new tablero(nombre, columnas, filas);
+		//los tableros en principio seran cuadrados asi que ponemos la medida como
+		//filas y como columnas
+		this._tablero = new tablero(nombre, medida, medida);
 		this._jugadores = new Map();
 	}
 
-	get tablero(){
+	get tablero() {
 		return this._tablero.info;
+	}
+
+	insertarRocas(cantidad) {
+		for (var i = 0; i < cantidad; i++) {
+			this._tablero.insertarRoca();
+		}
 	}
 
 	insertarTanque(tanque) {
 		this._tablero.insertarTanque(tanque);
 	}
 
-	movTanque(idTanque) {
-		this._tablero.mover(idTanque, "tanque");
+	movTanque(idJugador) {
+		this._tablero.mover(this._jugadores.get(idJugador), "tanque");
 	}
 
-	shootTanque(idTanque) {
-		this._tablero.disparar(idTanque);
+	shootTanque(idJugador) {
+		this._tablero.disparar(this._jugadores.get(idJugador));
 	}
 
-	girarTanque(idTanque, direccion) {
-		this._tablero.girar(idTanque, direccion);
+	girarTanque(idJugador, direccion) {
+		this._tablero.girar(this._jugadores.get(idJugador), direccion);
 	}
 
 	empezarPartida() {
 		let interval = setInterval(this._tablero.moverBalas, 500);
 	}
 
-	addJugador(idJugador,idTanque) {
-		this._jugadores.set(idJugador,idTanque);
+	addJugador(idJugador, idTanque) {
+		this._jugadores.set(idJugador, idTanque);
 	}
 
-	////seguir con esto
-	guardarPartida() {
-		var partida = {
-			id: this._id,
-			nombre:this._nombre,
-			dimensiones:this._tablero.dimensiones,
-			elemntos: this._tablero.info,
-			jugadores: Array.from(this._jugadores)
-		};
+	// ////seguir con esto
+	// guardarPartida() {
+	// 	var partida = {
+	// 		id: this._id,
+	// 		nombre:this._nombre,
+	// 		dimensiones:this._tablero.dimensiones,
+	// 		elemntos: this._tablero.info,
+	// 		jugadores: Array.from(this._jugadores)
+	// 	};
 
-		// var nom=cargarPartidas();
-		// var json=JSON.parse({partida:part});
-		// console.log(part);
-		mongoClient.connect(url, function(err, db) {
-			assert.equal(null, err);
-			console.log('conexion exitosa');
+	// 	// var nom=cargarPartidas();
+	// 	// var json=JSON.parse({partida:part});
+	// 	// console.log(part);
+	// 	mongoClient.connect(url, function(err, db) {
+	// 		assert.equal(null, err);
+	// 		console.log('conexion exitosa');
 
-			var conex = db.collection('partida');
-			// console.log(conex);
-			conex.insert(partida, function() {
-				console.log('insertado');
-				db.close();
-			})
-		})
-	}
-
-	cargarPartidas() {
-		mongoClient.connect(url, function(err, db) {
-			assert.equal(null, err);
-
-			var coleccion = db.collection('partida');
-			coleccion.find().toArray(function(err, data) {
-				return (data);
-				db.close();
-			});
-
-		})
-	}
-
-	comprobarPartida(nombrep, cb) {
-		mongoClient.connect(url, function(err, db) {
-			assert.equal(null, err);
-
-			var coleccion = db.collection('partida');
-			coleccion.find({
-				nombre: nombrep
-			}).toArray(function(err, data) {
-				db.close();
-				cb(data.length);
-			});
-
-		})
-	}
-
+	// 		var conex = db.collection('partidas');
+	// 		// console.log(conex);
+	// 		conex.insert(partida, function() {
+	// 			console.log('insertado');
+	// 			db.close();
+	// 		})
+	// 	})
+	// }
 }
 module.exports = Partida;
